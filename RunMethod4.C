002ca58a2e5239bf09d1fe5243b1803640b3cb16
SmithJK <smith.jenna.kathleen@gmail.com>
2019-08-06 12:10:20 -0700

    Small comment modification to RunMethod4.C

--- RunMethod4.C
+++ RunMethod4.C
@@ -9,7 +9,8 @@
    SetupAC();
    TGraphAsymmErrors* graph = ac->CreateGraphFromHst(ac_2013_586,false,false);
 
-   // 0.9560 and 0.0008 are the 16-clover parametrized beta and gamma for 586-2013
+   // 0.9560 and 0.849 are the 16-clover parametrized beta and gamma for 586-2013
+   // 0.0008 and 0.002 are the 16-clover parametrized beta and gamma uncertainties for 586-2013
    Method4a2a4(graph,0.9560,0.0008,0.849,0.002,false,true);
    //Method4mixing(graph,0.9560,0.0008,0.849,0.002,"results4.txt",1,3,7);
 }
